5a203e947a6b02b77611eee539c2fe80af5f1bfd
Luiz Ferreira <luizbionicle2647@gmail.com>
2022-11-18 21:12:10 -0300

    Otimiza código

--- monopoly_letra_e.py
+++ monopoly_letra_e.py
@@ -12,32 +12,27 @@ def matrizTransicao():
 def probablidadesAposTempo(t:int):
     m = matrizTransicao()
 
-    posInicial = np.zeros([123])
-    posInicial[0] = 1
+    posicao = np.zeros([123])
+    posicao[0] = 1
 
     contador = 0
     while contador < t:
-        posInicial = posInicial.dot(m)
+        posicao = posicao.dot(m)
         contador += 1
 
-    r = []
+    probablidades = []
 
-    for i in range(0, 40):
-        z1 = posInicial[i]
-        z2 = posInicial[i+40]
-        z3 = posInicial[i+80]
+    for i in range(0, 123):
+        z = posicao[i]
 
-        z = z1 + z2 + z3
+        if (i >= 40 and i < 120):
+            probablidades[i%40] += z
+        elif (i >= 120):
+            probablidades[20] += z
+        else:
+            probablidades.append(z)
 
-        if (i == 20):
-            z += posInicial[120]
-            z += posInicial[121]
-            z += posInicial[122]
-
-        r.append(z)
-
-    return r
-
+    return probablidades
 
 def posicaoZ(z, n=1000000):
     probs = probablidadesAposTempo(n)
@@ -46,6 +41,6 @@ def posicaoZ(z, n=1000000):
 
     return p
 
-z = posicaoZ(39)
+z = posicaoZ(20, 100000)
 
 print(z)
